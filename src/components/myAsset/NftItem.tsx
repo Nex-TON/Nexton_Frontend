fe2644b00f6f81bf68b1ed7088f4aef0e4241e3b
woogisea <rgn95652@gmail.com>
2023-08-24 16:09:15 +0900

    style : add Nft info date

--- src/components/myAsset/NftItem.tsx
+++ src/components/myAsset/NftItem.tsx
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 import NFTOngoing from "../../assets/image/NftOngoing.png";
 
 const NftItem = () => {
@@ -9,6 +9,9 @@ const NftItem = () => {
         alt="NFTOngoing"
         style={{ width: "100%", height: "100%" }}
       />
+      <NFTDDayText>D-Day</NFTDDayText>
+      <NFTExpiredDateText>Expired Date</NFTExpiredDateText>
+      <NFTExpiredDateText date>dd.mm.yy</NFTExpiredDateText>
     </NFTItemWrapper>
   );
 };
@@ -16,14 +19,37 @@ const NftItem = () => {
 export default NftItem;
 
 const NFTItemWrapper = styled.div`
+  position: relative;
   width: 100%;
   height: 16rem;
 
   border-radius: 1rem;
-
-  background-color: black;
 `;
 
 const NFTImage = styled.img`
   border-radius: 1rem;
+`;
+
+const NFTDDayText = styled.span`
+  position: absolute;
+  top: 1.3rem;
+  left: 1.3rem;
+
+  color: #fff;
+  ${({ theme }) => theme.fonts.Telegram_SemiBold};
+`;
+
+const NFTExpiredDateText = styled.span<{ date?: boolean }>`
+  position: absolute;
+  bottom: 2.5rem;
+  left: 1.3rem;
+
+  color: #fff;
+  ${({ theme }) => theme.fonts.Telegram_Caption_2};
+
+  ${({ date }) =>
+    date &&
+    css`
+      bottom: 1rem;
+    `}
 `;
