e6fc9b9c2d7f42a9aa3ff8fa5f58f5f80ae1a698
woogisea <rgn95652@gmail.com>
2023-08-25 16:17:28 +0900

    feat : back button switch case

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -1,7 +1,8 @@
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 import IcBack from "../../assets/icons/ic_back.svg";
 
 interface BackButtonProps {
+  type?: string;
   margin?: boolean;
   unstaking?: boolean;
   handleMovePreview?: () => void;
@@ -9,7 +10,8 @@ interface BackButtonProps {
 }
 
 const BackButton = (props: BackButtonProps) => {
-  const { margin, unstaking, handleMovePreview, handleMoveUnstaking } = props;
+  const { type, margin, unstaking, handleMovePreview, handleMoveUnstaking } =
+    props;
 
   const handleBack = () => {
     if (margin) {
@@ -22,28 +24,40 @@ const BackButton = (props: BackButtonProps) => {
   };
 
   return (
-    <BackButtonWrapper onClick={handleBack}>
-      <img src={IcBack} alt="back" width={10} />
+    <BackButtonWrapper onClick={handleBack} type={type}>
+      <BackImage src={IcBack} alt="back" type={type} />
     </BackButtonWrapper>
   );
 };
 
 export default BackButton;
 
-const BackButtonWrapper = styled.div`
+const BackButtonWrapper = styled.div<{ type: string }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: absolute;
-  top: 2.6rem;
-  left: 1rem;
-
-  width: 3.4rem;
-  height: 3.4rem;
+  ${({ type }) =>
+    type === "detail"
+      ? css`
+          position: relative;
+          width: 2.6rem;
+          height: 2.6rem;
+        `
+      : css`
+          position: absolute;
+          top: 2.6rem;
+          left: 1rem;
+          width: 3.4rem;
+          height: 3.4rem;
+        `}
 
   border-radius: 50%;
   background-color: #f9f9ff;
   box-shadow: 0px 0px 20px 0px #e1e4e6;
 
   cursor: pointer;
+`;
+
+const BackImage = styled.img<{ type: string }>`
+  width: ${({ type }) => (type === "detail" ? "0.8rem" : "1rem")};
 `;
