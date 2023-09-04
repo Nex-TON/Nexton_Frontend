d9487fe255ba98fd4b4ea7aa74f4bf4e516aa5d8
woogisea <rgn95652@gmail.com>
2023-09-04 16:48:08 +0900

    feat : change DDay function

--- src/utils/dateChanger.ts
+++ src/utils/dateChanger.ts
@@ -8,14 +8,37 @@ export const dateChangerShorter = (dateString: string) => {
   const month = String(dateObj.getMonth() + 1).padStart(2, "0");
   const day = String(dateObj.getDate()).padStart(2, "0");
 
-  const formattedDate = `${day}/${month}/${year}`;
-
-  return formattedDate;
+  return `${day}/${month}/${year}`;
 };
 
+export const expiredDateChanger = (dateString: string, lockPeriod: number) => {
+  const dateObj = new Date(dateString);
+  dateObj.setDate(dateObj.getDate() + lockPeriod);
+  const year = String(dateObj.getFullYear()).slice(-2);
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+  const day = String(dateObj.getDate()).padStart(2, "0");
+
+  return `${day}.${month}.${year}`;
+};
+
+export const DDayChange = (dateString: string, lockPeriod: number) => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const dateObj = new Date(dateString);
+  dateObj.setDate(dateObj.getDate() + lockPeriod);
+  dateObj.setHours(0, 0, 0, 0);
+
+  let differenceInMs = dateObj.getTime() - today.getTime();
+
+  // Convert the difference to days
+  let days = differenceInMs / (1000 * 60 * 60 * 24);
+
+  return days;
+};
 // 날짜 계산
 
-function formatDate(date: Date, type?: string): string {
+export function formatDate(date: Date, type?: string): string {
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = String(date.getFullYear()).slice(-2);
