7fc3a163d71413f9f4b6becd81b100b4faa88982
woogisea <rgn95652@gmail.com>
2023-09-06 17:32:14 +0900

    feat : Nominator constants

---
+++ src/constants/Nominator.ts
@@ -0,0 +1,18 @@
+export const NOMINATOR_LIST = [
+  {
+    title: "KoreaTon Pool #1",
+    totalStake: 822.056,
+    ValidatorStake: 19.082,
+    NominatorStake: 802.974,
+    type: "profit",
+    check: false,
+  },
+  {
+    title: "DG Pool #1",
+    totalStake: 937.263,
+    ValidatorStake: 36.115,
+    NominatorStake: 901.149,
+    type: "nonProfit",
+    check: false,
+  },
+];
