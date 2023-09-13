d64801e9e804f37f80b4083f053de041815d906f
woogisea <rgn95652@gmail.com>
2023-09-13 17:36:32 +0900

    feat : modal constants text

---
+++ src/constants/ModalText.ts
@@ -0,0 +1,26 @@
+export const MODAL_TEXT = {
+  stake: {
+    title: "Successfully Staked",
+    desc: "For now you can check minted NFT",
+    descBr: "on My asset, and transaction data",
+  },
+  loan: {
+    title: "Successfully borrowed NXT",
+    desc: "Please check deposited NXT",
+    descBr: "in your wallet, and transaction",
+    descBr2: "history by tapping below button",
+  },
+  unstaking: {
+    title: "Successfully",
+    titleBr: "Unstaking Requested",
+    desc: "Unstaking process would take about",
+    descBr: "9hours. Check left time on My asset",
+    descBr2: "and transaction data on TON viewer",
+  },
+  claim: {
+    title: "Succeed!",
+    desc: "Unstaked amount is claimed",
+    descBr: "and will be deposited into your wallet",
+    descBr2: "It could take a few minutes. Check it!",
+  },
+};
