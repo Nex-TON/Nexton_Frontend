5b0a4d5c66a26ff9547948af984e734d75800b1d
woogisea <rgn95652@gmail.com>
2023-09-15 17:25:54 +0900

    feat : common swr fetcher

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,3 +3,6 @@ import axios from "axios";
 export const client = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}`,
 });
+
+export const nextonFetcher = (url: string) =>
+  client.get(url).then((res) => res.data);
