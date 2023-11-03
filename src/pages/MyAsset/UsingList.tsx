404894e09bead28ca462250691aa68af8e917a57
jeongkite <jeong6kite@gmail.com>
2023-11-03 16:31:30 +0900

    feat : My Asset Using filter

--- src/pages/MyAsset/UsingList.tsx
+++ src/pages/MyAsset/UsingList.tsx
@@ -1,7 +1,28 @@
-import styled from "styled-components";
+import { useState } from "react";
+import styled, { css } from "styled-components";
 
 const UsingList = () => {
-  return <NFtListWrapper></NFtListWrapper>;
+  const [isSelect, setIsSelect] = useState([true, false, false]);
+
+  const handleSelect = (index: number) => {
+    setIsSelect(isSelect.map((item, idx) => (idx === index ? true : false)));
+  };
+
+  return (
+    <NFtListWrapper>
+      <NftListHeader>
+        <NFTSelectBox $active={isSelect[0]} onClick={() => handleSelect(0)}>
+          All
+        </NFTSelectBox>
+        <NFTSelectBox $active={isSelect[1]} onClick={() => handleSelect(1)}>
+          Listed
+        </NFTSelectBox>
+        <NFTSelectBox $active={isSelect[2]} onClick={() => handleSelect(2)}>
+          Collateralized
+        </NFTSelectBox>
+      </NftListHeader>
+    </NFtListWrapper>
+  );
 };
 
 export default UsingList;
@@ -27,4 +48,44 @@ const NFtListWrapper = styled.div`
     margin-bottom: 3px;
     background-clip: padding-box;
   }
+`;
+
+const NftListHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+
+  width: 100%;
+  padding: 1rem;
+
+  border-radius: 3rem;
+  background-color: #f2f2f7;
+`;
+
+const NFTSelectBox = styled.button<{ $active?: boolean }>`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  padding: 0.8rem 1.4rem;
+
+  border: none;
+  border-radius: 2rem;
+  background-color: #f9f9ff;
+
+  ${({ theme }) => theme.fonts.Nexton_Label_Small};
+
+  ${({ $active }) =>
+    $active
+      ? css`
+          background-color: #333;
+          color: #fff;
+        `
+      : css`
+          background-color: #fff;
+          color: #5d5e67;
+        `}
+
+  outline: none;
+  cursor: pointer;
 `;
