5e5bdbbd8db04573a3ac26e5b4b2be48273f7509
jeongkite <jeong6kite@gmail.com>
2024-02-14 16:02:46 +0900

    update: ton manifest file url

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 const manifestUrl =
-  'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+  "https://raw.githubusercontent.com/jeongkite/ton-test/main/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <App />
-    </TonConnectUIProvider>
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>
 );
