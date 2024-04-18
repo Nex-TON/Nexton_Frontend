9761abc1a0e65596699ad45549b30d947a5710a2
Kirill Bondarenko <billionmann@gmail.com>
2024-04-18 18:15:43 +1000

    fix: removed loacalstorage logic & updated deployment env-s #2

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 
 import Router from "./components/common/Router";
-import { isProduction, network } from "./hooks/contract/useTonClient";
+import { network } from "./hooks/contract/useTonClient";
 import GlobalStyle from "./styles/globalStyles";
 import theme from "./styles/theme";
 
-console.log(`You're connected to the ${isProduction ? "mainnet" : network} network!`);
+console.log(`You're connected to the ${network} network!`);
 
 const App = () => {
   return (
--- src/hooks/contract/useTonClient.ts
+++ src/hooks/contract/useTonClient.ts
@@ -3,15 +3,13 @@ import { TonClient } from "@ton/ton";
 
 import { useAsyncInitialize } from "./useAsyncInitialize";
 
-export const isProduction = import.meta.env.PROD;
-
 export const network = (import.meta.env.VITE_TON_NETWORK as "mainnet" | "testnet") || "testnet";
 
 export function useTonClient(): TonClient | undefined {
   return useAsyncInitialize(
     async () =>
       new TonClient({
-        endpoint: await getHttpEndpoint({ network: isProduction ? "mainnet" : network }),
+        endpoint: await getHttpEndpoint({ network }),
       }),
   );
 }
