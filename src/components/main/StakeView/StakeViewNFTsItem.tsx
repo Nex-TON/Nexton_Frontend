d9b46578b65b7406211498feefdb5c12d24c0302
Kirill Bondarenko <billionmann@gmail.com>
2024-05-19 13:37:29 +1000

    refactor/staking-tab-qa revision

--- src/components/main/StakeView/StakeViewNFTsItem.tsx
+++ src/components/main/StakeView/StakeViewNFTsItem.tsx
@@ -24,7 +24,7 @@ const StakeViewNFTsItem = (props: MainNftViewItemProps) => {
   const handleMoveNftDetail = (event: React.MouseEvent<HTMLDivElement>) => {
     const rect = (event.target as HTMLImageElement).getBoundingClientRect();
     setImageSize({ width: rect?.width * 1.2, height: rect?.height * 1.3 });
-    navigate(`/myasset/${nftId}`);
+    navigate(`/myasset/${nftId}`, { state: { prev: "/" } });
   };
 
   return DDayChange(timeStamp, lockPeriod) > 15 ? (
