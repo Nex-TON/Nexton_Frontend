b796316ec7c963a9db644b2e0eebdaab8d0147c2
jeongkite <jeong6kite@gmail.com>
2024-06-14 10:49:47 +0900

    update: eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,38 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'simple-import-sort'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "simple-import-sort"],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     // increase the severity of rules so they are auto-fixable
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-explicit-any": "off",
   },
   overrides: [
     // override "simple-import-sort" config
     {
-      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
       rules: {
-        'simple-import-sort/imports': [
-          'error',
+        "simple-import-sort/imports": [
+          "error",
           {
             groups: [
               // Packages `react` related packages come first.
-              ['^react', '^@?\\w'],
+              ["^react", "^@?\\w"],
               // Internal packages.
-              ['^(@|components)(/.*|$)'],
+              ["^(@|components)(/.*|$)"],
               // Side effect imports.
-              ['^\\u0000'],
+              ["^\\u0000"],
               // Parent imports. Put `..` last.
-              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
               // Other relative imports. Put same-folder imports and `.` last.
-              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
               // Style imports.
-              ['^.+\\.?(css)$'],
+              ["^.+\\.?(css)$"],
             ],
           },
         ],
