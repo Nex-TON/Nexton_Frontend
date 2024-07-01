3862c452e837aaec22544f83cab128280403766f
Kirill Bondarenko <billionmann@gmail.com>
2024-07-01 19:05:18 +1000

    wip: charts added (fix 3)

--- src/utils/limitDecimals.ts
+++ src/utils/limitDecimals.ts
@@ -5,6 +5,8 @@
  * @returns {string} - The formatted number as a string.
  */
 export function limitDecimals(value, maxDecimals) {
+  if (!value) return value;
+
   const [integerPart, decimalPart] = value.toString().split(".");
 
   // If there's no decimal part or its length is within the limit, return the value as is
