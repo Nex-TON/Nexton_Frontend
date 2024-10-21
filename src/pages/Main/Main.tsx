e1af983073c65ad2bf819d724ba26502be33e5c6
jymin99 <min10909@naver.com>
2024-10-22 01:46:04 +0900

    feat/main page floating button add successfully

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,12 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { useLocation } from "react-router-dom";
 import { Slide, toast, ToastContainer } from "react-toastify";
 import styled from "styled-components";
 import { mutate } from "swr";
-import { Fab, Action } from "react-tiny-fab";
-import "react-tiny-fab/dist/styles.css";
+// import {Fab,Action} from "react-tiny-fab";
+// import "react-tiny-fab/dist/styles.css";
 import React from "react";
+import { Fab, Zoom } from "@mui/material";
+import { Popover } from "@mui/material";
 
 import Header from "@/components/common/Header";
 import ActionCards from "@/components/main/ActionCards";
@@ -17,14 +18,28 @@ import { useTrackReferral } from "@/hooks/api/referral/useTrackReferral";
 import { useStakeInfo } from "@/hooks/api/useStakeInfo";
 import useTonConnect from "@/hooks/contract/useTonConnect";
 import FloatingOpenIc from "@/assets/icons/Main/floating_open.svg";
+import FloatingCloseIc from "@/assets/icons/Main/floating_close.svg";
 import FloatCommunityIc from "@/assets/icons/Main/floating_community.svg";
 import FloatSupportIc from "@/assets/icons/Main/floating_support.svg";
+import SupportBallon from "@/assets/image/support_ballon.png";
+import CommunityBallon from "@/assets/image/community_ballon.png";
 
 import "react-toastify/dist/ReactToastify.css";
+import { fontFamily, fontSize } from "@mui/system";
 
 const tele = (window as any).Telegram.WebApp;
 
 const Main: React.FC = () => {
+  const [isFbOpen, setIsFbOpen] = useState(false);
+
+  const handleFloatingButton = () => {
+    setIsFbOpen(prev => !prev);
+  };
+
+  const closeFab = () => {
+    setIsFbOpen(false);
+  };
+
   const { address, balance, refreshTonData, connected, tonConnectUI } = useTonConnect();
   const { nftList, isLoading, isError } = useStakeInfo(address);
 
@@ -139,7 +154,6 @@ const Main: React.FC = () => {
   return (
     <>
       {modal && <WelcomeModal toggleModal={toggleModal} />}
-
       <MainWrapper>
         <Header isOpen={false} text="NEXTON" backgroundType={false} connected={connected} tonConnectUI={tonConnectUI} />
         <MainMyAssetInfo
@@ -158,19 +172,62 @@ const Main: React.FC = () => {
         {/* <StakeView /> */}
 
         <MyTokens />
+        <Overlay visible={isFbOpen} onClick={closeFab} />
         <Fab
-          mainButtonStyles={{ backgroundColor: "#1F53FF", width: "48px", height: "48px", padding: "12px" }}
-          style={{ bottom: "42px", right: "10px" }}
-          event="hover"
-          alwaysShowTitle={true}
-          icon={<img  src={FloatingOpenIc} style={{ width: "24px", height: "24px" }} />}
+          style={{
+            position: "absolute",
+            backgroundColor: "#1F53FF",
+            width: "48px",
+            height: "48px",
+            padding: "12px",
+            bottom: "42px",
+            right: "10px",
+          }}
+          onClick={handleFloatingButton}
         >
-          <Action text="Community" onClick={() => {window.open("https://t.me/+YBNeM9m_yhtlNzM9")}} style={{padding:"8px",height:"40px",width:"40px",backgroundColor:"#F8F8F8"}}>
-            <img src={FloatCommunityIc} style={{height:"24px", width:"24px"}} />
-          </Action>
-          <Action  text="Support" onClick={() => {window.open("https://t.me/m/-Y3bstHbMzE9")}} style={{padding:"8px",height:"40px",width:"40px",backgroundColor:"#F8F8F8"}}>
-          <img src={FloatSupportIc} style={{height:"24px", width:"24px"}}/>
-          </Action>
+          <img
+            src={isFbOpen ? FloatingCloseIc : FloatingOpenIc}
+            alt="Floating button"
+            style={{ width: "24px", height: "24px" }}
+          />
+          {isFbOpen && (
+            <>
+              <Zoom in={isFbOpen}>
+                  <Fab
+                    style={{
+                      backgroundColor: "#F8F8F8",
+                      padding: "8px",
+                      height: "40px",
+                      width: "40px",
+                      position: "absolute",
+                      bottom: "116px",
+                    }}
+                    onClick={() => {
+                      window.open("https://t.me/m/-Y3bstHbMzE9");
+                    }}
+                  >
+                    <img src={FloatSupportIc} alt="community link" />
+                  </Fab>
+              </Zoom>
+              <Zoom in={isFbOpen}>
+                <Fab
+                  style={{
+                    backgroundColor: "#F8F8F8",
+                    padding: "8px",
+                    height: "40px",
+                    width: "40px",
+                    position: "absolute",
+                    bottom: "66px",
+                  }}
+                  onClick={() => {
+                    window.open("https://t.me/+YBNeM9m_yhtlNzM9");
+                  }}
+                >
+                  <img src={FloatCommunityIc} alt="community link" />
+                </Fab>
+              </Zoom>
+            </>
+          )}
         </Fab>
       </MainWrapper>
 
@@ -192,6 +249,18 @@ const Main: React.FC = () => {
 };
 
 export default Main;
+
+const Overlay = styled.div<{ visible: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  opacity: 0.5;
+  background: var(--Neutral-Neutural-0, #000);
+  display: ${({ visible }) => (visible ? "block" : "none")};
+  z-index: 999; /* 오버레이가 Fab보다 위에 오도록 */
+`;
 
 const MainWrapper = styled.div`
   width: 100%;
