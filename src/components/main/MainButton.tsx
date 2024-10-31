8dd7445ae31c730988c2d5629f6ebe04ac222f5e
BWL <bwl@BWLui-MacBookPro.local>
2024-10-31 11:19:14 +0900

    test:remove useless code

--- src/components/main/MainButton.tsx
+++ src/components/main/MainButton.tsx
@@ -14,13 +14,6 @@ const MainButton = ({ style }: { style?: React.CSSProperties }) => {
     if (connected) {
       navigate("/stake/amount");
     } else {
-      // const connectWalletTag={
-      //   gtmId:'GTM-PFM27JKW',
-      //   dataLayer:{
-      //     event:"gtm.click",
-      //   },
-      // };
-      // TagManager.dataLayer(connectWalletTag);
       tonConnectUI.connectWallet();
     }
   };
