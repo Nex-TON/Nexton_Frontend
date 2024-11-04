35af7cd93a4dbb5091dc1ee9d1b7510d9576f5fc
BWL <bwl@BWLui-MacBookPro.local>
2024-11-04 15:33:59 +0900

    fix:palceholder 짤림 문제 해결

--- src/components/stake/Filter/TokenFilter.tsx
+++ src/components/stake/Filter/TokenFilter.tsx
@@ -10,7 +10,8 @@ import MenuItem from "@mui/material/MenuItem";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-
+import Option, { optionClasses } from "@mui/joy/Option";
+import ListItemDecorator, { listItemDecoratorClasses } from "@mui/joy/ListItemDecorator";
 
 const TokenFilter = () => {
   const [token, setToken] = React.useState("TON");
@@ -18,12 +19,17 @@ const TokenFilter = () => {
     setToken(event.target.value);
   };
   const [isOpen, setIsOpen] = React.useState(false);
-  const handleToggle = () => {
+  const handleToggle = (event: React.MouseEvent) => {
+    event.preventDefault(); // 기본 동작 방지
     setIsOpen(!isOpen);
   };
 
   const CustomIcon = ({ isOpen }: { isOpen: boolean }) => (
-    <img src={isOpen ? IcArrowUp : IcArrowDown} alt="toggle icon" style={{ width: '14px', height: '14px',marginRight:"10px" }} />
+    <img
+      src={isOpen ? IcArrowUp : IcArrowDown}
+      alt="toggle icon"
+      style={{ width: "14px", height: "14px", marginRight: "10px" }}
+    />
   );
 
   const selectWidth = token === "nxTON" ? "114px" : "98px";
@@ -31,31 +37,54 @@ const TokenFilter = () => {
   return (
     <div>
       <FormControl>
-        <Select 
-        value={token} 
-        onChange={handleChange} 
-        displayEmpty 
-        inputProps={{ "aria-label": "Whitout label" }} 
-        style={{ backgroundColor: "#1A1B23",borderRadius:"20px",height:"35px",width:selectWidth,display:"flex",textAlign:"start",outline:"none",border:"none"}}
-        onMouseDown={handleToggle}
-        IconComponent={() => <CustomIcon isOpen={isOpen} />}
-        MenuProps={{
-            PaperProps:{
-            style:{
-                backgroundColor:"#1A1B23",
-                borderRadius:"10px",
-                marginTop:"8px",
-                width:"119px",
-            }
-        }}}  
+        <Select
+          value={token}
+          onChange={handleChange}
+          displayEmpty
+          // inputProps={{ "aria-label": "Whitout label" }}
+          sx={{
+            backgroundColor: "#1A1B23",
+            borderRadius: "20px",
+            height: "35px",
+            width: selectWidth,
+            display: "flex",
+            textAlign: "start",
+            outline: "none",
+            border: "none",
+          }}
+          onMouseDown={handleToggle}
+          IconComponent={() => <CustomIcon isOpen={isOpen} />}
+          MenuProps={{
+            PaperProps: {
+              style: {
+                backgroundColor: "#1A1B23",
+                borderRadius: "10px",
+                marginTop: "8px",
+                width: "119px",
+              },
+            },
+          }}
+          slotProps={{
+            root: {
+              sx: {
+                "&:focus": {
+                  outline: "none", // 포커스 시 아웃라인 제거
+                  backgroundColor: "#1A1B23", // 포커스 시 배경색 유지
+                },
+                "&:hover": {
+                  backgroundColor: "#1A1B23", // 호버 시 배경색 유지
+                },
+              },
+            },
+          }}
         >
-          <MenuItem value="TON">
+          <MenuItem value="TON" style={{ backgroundColor: "#1A1B23" }}>
             <DefaultFilterWrapper>
               <img src={IcTon} alt="filter ton icon" />
               <DefaultValueText>TON</DefaultValueText>
             </DefaultFilterWrapper>
           </MenuItem>
-          <MenuItem value="nxTON">
+          <MenuItem value="nxTON" style={{ backgroundColor: "#1A1B23" }}>
             <DefaultFilterWrapper>
               <img src={IcNxTon} alt="filter nxton icon" />
               <DefaultValueText>nxTON</DefaultValueText>
