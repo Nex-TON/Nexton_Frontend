8c955cda907de1dbbe196567678a6a87ee7cb6dc
BWL <bwl@BWLui-MacBookPro.local>
2024-11-04 16:21:10 +0900

    feat:change contract and fix placeholder

--- src/pages/Stake/Amount.tsx
+++ src/pages/Stake/Amount.tsx
@@ -120,7 +120,7 @@ const Amount = () => {
           error={errors.amount?.message as string}
           disabled={!connected}
           tokenLabel={<TokenFilter/>}
-          placeholder="min 1 TON"
+          placeholder="Stake (min.1)"
           balance={balance}
           convertAmount={convertAmount}
         />
