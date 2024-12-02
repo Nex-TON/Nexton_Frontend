333f736510159cb57fc9fd2d331ee2f1faa041a3
jymin99 <min10909@naver.com>
2024-12-02 14:19:32 +0900

    feat: my page에 total balance nxTon 추가:

--- src/components/myAsset/TotalBalance.tsx
+++ src/components/myAsset/TotalBalance.tsx
@@ -3,7 +3,7 @@ import { mutate } from "swr";
 
 import useTonConnect from "@/hooks/contract/useTonConnect";
 import IcTon from "@/assets/icons/MyAsset/ic_tonSymbol.svg";
-import { numberCutter } from "@/utils/numberCutter";
+import IcnxTon from "@/assets/icons/MyAsset/ic_nxTonSymbol.svg";
 import { useEffect, useState } from "react";
 
 export const TotalBalance = () => {
@@ -33,23 +33,59 @@ export const TotalBalance = () => {
     <TotalBalanceWrapper>
       <TotalBalanceHeader>Total Balance</TotalBalanceHeader>
       <TotalBalanceBoxWrapper>
-        <img src={IcTon} alt="my asset page ton logo" />
-        <TotalBalanceText>
-          <SideText>Balance</SideText>
-          <Balance>
-            {isRefreshing ? (
-              <Balance>-.---</Balance>
-            ) : (
-              <>
-                <Balance>{balance === 0 || balance ? balance?.toFixed(3) : "0.000"}TON</Balance>
-              </>
-            )}
-          </Balance>
-        </TotalBalanceText>
+        <TokenWrapper>
+          <img src={IcTon} alt="my asset page ton logo" />
+          <TotalBalanceText>
+            <SideText>Balance</SideText>
+            <Balance>
+              {isRefreshing ? (
+                <Balance>-.---</Balance>
+              ) : (
+                <>
+                  <Balance>{balance === 0 || balance ? balance?.toFixed(3) : "0.000"} TON</Balance>
+                </>
+              )}
+            </Balance>
+          </TotalBalanceText>
+        </TokenWrapper>
+        <DivideLine />
+        <TokenWrapper>
+          <img src={IcnxTon} alt="my asset page ton logo" />
+          <TotalBalanceText>
+            <SideText>Balance</SideText>
+            <Balance>
+              {isRefreshing ? (
+                <Balance>-.---</Balance>
+              ) : (
+                <>
+                  <Balance>{balance === 0 || balance ? balance?.toFixed(3) : "0.000"} nxTON</Balance>
+                </>
+              )}
+            </Balance>
+          </TotalBalanceText>
+        </TokenWrapper>
       </TotalBalanceBoxWrapper>
     </TotalBalanceWrapper>
   );
 };
+
+const DivideLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background: #e5e5ea;
+`;
+
+const TokenWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  gap: 1.7rem;
+  img {
+    width: 43px;
+    height: 43px;
+  }
+`;
 
 const Balance = styled.div`
   color: #303234;
@@ -69,20 +105,15 @@ const TotalBalanceText = styled.div`
 
 const TotalBalanceBoxWrapper = styled.div`
   display: flex;
-  flex-direction: row;
-  gap: 1.7rem;
-  align-items: center;
+  flex-direction: column;
+  gap: 2.2rem;
 
   margin-bottom: 5.5rem;
   width: 100%;
-  height: 8.5rem;
+  height: auto;
   box-shadow: 0px 0px 12px 0px rgba(206, 216, 225, 0.5);
   border-radius: 15px;
-  padding: 2rem;
-  img {
-    width: 43px;
-    height: 43px;
-  }
+  padding: 2rem 2.2rem 2.7rem 1.9rem;
 `;
 
 const TotalBalanceHeader = styled.div`
