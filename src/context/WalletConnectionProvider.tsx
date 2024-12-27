9966ffca1034948f3dfa9a88b37a6a2b774d091d
MoonSeoHyeong <moonsh7648@gmail.com>
2024-12-27 11:35:03 +0900

    fix: persist connection when refreshed

--- src/context/WalletConnectionProvider.tsx
+++ src/context/WalletConnectionProvider.tsx
@@ -21,6 +21,16 @@ export const WalletProvider: React.FC<{ children: ReactNode }> = ({ children }) 
   const tonConnect = useTonConnect();
   const tomoWallet = useTomoWallet();
 
+  useEffect(() => {
+    if (!activeWalletType) {
+      const savedWalletType = localStorage.getItem("walletType") as WalletTypes | null;
+      if (savedWalletType) {
+        if (isConnectionValid(savedWalletType)) setActiveWalletType(savedWalletType);
+        else localStorage.clear();
+      }
+    }
+  }, [activeWalletType]);
+
   // activeWalletType에 따라 해당 훅 반환
   const getActiveWallet = () => {
     if (activeWalletType === "TonConnect") return tonConnect;
@@ -31,6 +41,7 @@ export const WalletProvider: React.FC<{ children: ReactNode }> = ({ children }) 
   const connect = (type: WalletTypes) => {
     if (type === "TonConnect") tonConnect.tonConnectUI.openModal();
     if (type === "Tomo") tomoWallet.openConnectModal();
+    localStorage.setItem("walletType", type);
   };
 
   return (
@@ -38,6 +49,23 @@ export const WalletProvider: React.FC<{ children: ReactNode }> = ({ children }) 
       {children}
     </WalletContext.Provider>
   );
+};
+
+const isConnectionValid = (type: WalletTypes): boolean => {
+  if (type === "Tomo") {
+    if (!localStorage.getItem("tomo-tg-wallet-sdk-lastTime_")) return false;
+    if (!localStorage.getItem("tomo-tg-wallet-sdk-account_")) return false;
+    if (!localStorage.getItem("tomo-tg-wallet-sdk-accounts_")) return false;
+    if (!localStorage.getItem("tomo-tg-wallet-sdk-connect_type_")) return false;
+    return true;
+  } else if (type === "TonConnect") {
+    if (!localStorage.getItem("ton-connect-ui_last-selected-wallet-info")) return false;
+    if (!localStorage.getItem("ton-connect-storage_http-bridge-gateway::https://bridge.tonapi.io/bridge")) return false;
+    if (!localStorage.getItem("ton-connect-ui_wallet-info")) return false;
+    if (!localStorage.getItem("ton-connect-ui_preferred-wallet")) return false;
+    return true;
+  }
+  return false;
 };
 
 // Custom Hook
