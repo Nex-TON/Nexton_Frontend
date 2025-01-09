24957518de608faf6a8139f2b13db7eb49af44df
Yeong-si <dtd1110@gmail.com>
2025-01-09 13:29:58 +0900

    fix: error handling

--- src/components/main/common/Button.tsx
+++ src/components/main/common/Button.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 import IcMenuCoin from "../../../assets/icons/Landing/ic_menuCoin.svg";
