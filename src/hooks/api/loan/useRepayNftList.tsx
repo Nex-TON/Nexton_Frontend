0b6431e7c353c6658cbe916016400b16bac6dba5
MoonSeoHyeong <moonsh7648@gmail.com>
2025-02-03 15:13:27 +0900

    fix: conditional useSWR

--- src/hooks/api/loan/useRepayNftList.tsx
+++ src/hooks/api/loan/useRepayNftList.tsx
@@ -1,23 +1,17 @@
 import useSWR from "swr";
 import { borrowNftInfo } from "@/types/ReapyNftList";
 import { nextonFetcher } from "@/api/axios";
-import { boolean } from "zod";
 
-export const useRepayNftList = (address: string) => {
-  if (!address) {
-    return {
-      nftList: [],
-      isLoading: false,
-      isError: false,
-    };
-  }
+export const useRepayNftList = (address: string | null | undefined) => {
+  const shouldFetch = address ? `/data/lendingList?address=${address}` : null;
 
-  const { data, isLoading, error } = useSWR<borrowNftInfo[]>(`/data/lendingList?address=${address}`, nextonFetcher, {
+  const { data, isLoading, error } = useSWR<borrowNftInfo[]>(shouldFetch, nextonFetcher, {
     errorRetryCount: 3,
   });
+
   return {
-    borrowList: data,
-    isLoading,
-    isError: boolean(error),
+    borrowList: data ?? [], // 데이터가 없을 경우 빈 배열 반환
+    isLoading: shouldFetch ? isLoading : false, // fetch가 없을 경우 로딩 상태 false
+    isError: shouldFetch ? Boolean(error) : false, // fetch가 없을 경우 에러 false
   };
 };
