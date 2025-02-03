59b8a98c4759e55b069a088a31230ae3f3460eac
MoonSeoHyeong <moonsh7648@gmail.com>
2025-02-03 10:35:52 +0900

    fix: null address fetch

--- src/hooks/api/useStakeInfo.tsx
+++ src/hooks/api/useStakeInfo.tsx
@@ -3,7 +3,15 @@ import useSWR from "swr";
 import { nextonFetcher } from "@/api/axios";
 import { nftInfo } from "@/types/Nft";
 
-export const useStakeInfo = (address: string) => {
+export const useStakeInfo = (address: string | null | undefined) => {
+  if (!address) {
+    return {
+      nftList: [],
+      isLoading: false,
+      isError: false,
+    };
+  }
+
   const { data, isLoading, error } = useSWR<nftInfo[]>(
     `/data/getAllStakeInfoByAddress?address=${address}`,
     nextonFetcher,
