a108d69384dabfe4a43598fee57b17485fc9f8b9
shinji530 <shinji53059@gmail.com>
2025-03-11 16:24:31 +0900

    fix:fix modal

--- src/components/main/Modal/PopupModal.tsx
+++ src/components/main/Modal/PopupModal.tsx
@@ -43,7 +43,7 @@ export const PopupModal = (props: PopupProps) => {
                   console.log(window.Telegram.Webapp);
                 } else {
                   console.log('error');
-                  toggleModal();
+                  // toggleModal();
                 }
               }}
             >
