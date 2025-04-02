c9702c2d706ce2ad5fb9b934a519f897ef70d240
kuk329 <kuk329@cau.ac.kr>
2025-04-02 13:37:57 +0900

    add_betaRouter

--- src/hooks/contract/depositTon.js
+++ src/hooks/contract/depositTon.js
@@ -38,6 +38,7 @@ function depositTon() {
     },
     strategyDeposit: async (data, value) => {
       const strategy_handler = mapStrategyHandler(data.strategy);
+      console.log(strategy_handler);
       if (nextonContract && strategy_handler) {
         return await nextonContract.send(
           sender,
