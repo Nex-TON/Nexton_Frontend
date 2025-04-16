9f3d9a286e818d9b38da7734bd6766fa4058f379
ddis1004 <moonsh7648@gmail.com>
2025-04-16 11:26:35 +0900

    feat: evaa contract message

--- src/hooks/contract/utils.ts
+++ src/hooks/contract/utils.ts
@@ -40,3 +40,40 @@ export function stringToAmount(token: string, amount: string) {
       return toNano(amount) / 1000n;
   }
 }
+
+export function mapStrategyHandler(handler) {
+  if (import.meta.env.VITE_TON_NETWORK === "mainnet")
+    if (handler === "Bemo Pool") return Address.parse("EQDH13TaSeQVHHf9YOeG4y3A0VrkYn8y24WESaJIui3BujDc");
+  if (import.meta.env.VITE_TON_NETWORK === "testnet") {
+    if (handler === "Bemo Pool") return Address.parse("kQDyV2q5-epazG6SffES6v9QPlNzeFC19uy71TPjXwXd0bLg");
+    if (handler === "Evaa Pool") return Address.parse("kQBcQ1g65ebK7c8AJXTkgivJkiltU99iPJ5d8wGSYmCafLrK");
+  }
+}
+
+export function mapStrategyFee(handler) {
+  if (import.meta.env.VITE_TON_NETWORK === "mainnet")
+    if (handler === "Bemo Pool")
+      return {
+        fee: toNano(0.2),
+        fwd: toNano(0.1),
+      };
+  if (handler === "Evaa Pool")
+    return {
+      fee: toNano(0.5),
+      jettonFwd: toNano(0.45),
+      fwd: toNano(0.45),
+    };
+  if (import.meta.env.VITE_TON_NETWORK === "testnet") {
+    if (handler === "Bemo Pool")
+      return {
+        fee: toNano(0.2),
+        fwd: toNano(0.1),
+      };
+    if (handler === "Evaa Pool")
+      return {
+        fee: toNano(0.5),
+        jettonFwd: toNano(0.45),
+        fwd: toNano(0.45),
+      };
+  }
+}
