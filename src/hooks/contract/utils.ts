7633e23386253e0a4bda7f9707a35cf2cce3dc3b
ddis1004 <moonsh7648@gmail.com>
2025-06-04 16:59:16 +0900

    update: added bmTon address

--- src/hooks/contract/utils.ts
+++ src/hooks/contract/utils.ts
@@ -1,6 +1,6 @@
 import { Address, fromNano, toNano } from "@ton/core";
 
-export function mapTokenMasterAddress(token) {
+export function mapTokenMasterAddress(token: string) {
   switch (token) {
     case "nxTON":
       return Address.parse(import.meta.env.VITE_NXTON_MASTER);
@@ -9,6 +9,11 @@ export function mapTokenMasterAddress(token) {
         return Address.parse("EQCdEj1dEh76-Qacc38ZRH2eGtqyp-50fO3_0wBKF8HKT9zh");
       else if (import.meta.env.VITE_TON_NETWORK == "testnet")
         return Address.parse("kQAUupHzEYK1B9yvg9qhaGFJqF-EcAgW58HjDs438pSex9Gu");
+    case "bmTON":
+      if (import.meta.env.VITE_TON_NETWORK == "mainnet")
+        return Address.parse("EQCSxGZPHqa3TtnODgMan8CEM0jf6HpY-uon_NMeFgjKqkEY");
+      else if (import.meta.env.VITE_TON_NETWORK == "testnet")
+        return Address.parse("kQDpsupsgWJYYSdsTrg_is5Z8hkfHJqlH5zGqby7oWunvwPz");
     case "USDT":
       if (import.meta.env.VITE_TON_NETWORK == "mainnet")
         return Address.parse("EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs");
@@ -20,6 +25,7 @@ export function mapTokenMasterAddress(token) {
 
 export function amountToString(token: string, amount: bigint) {
   switch (token) {
+    case "bmTON":
     case "nxTON":
     case "TON":
       return fromNano(amount);
@@ -35,6 +41,7 @@ export function stringToAmount(token: string, amount: string) {
   switch (token) {
     case "nxTON":
     case "TON":
+    case "bmTON":
       return toNano(amount);
     case "USDT":
       return toNano(amount) / 1000n;
